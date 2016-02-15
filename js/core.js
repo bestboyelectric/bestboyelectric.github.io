5b904c6434ed42816d7ce980ea54814782f49b3a
bodar77 <cbower@live.co.uk>
2016-02-15 00:25:48 +0000

    Electric Website IVELECWEB-17
    Adding logos to site

--- js/core.js
+++ js/core.js
@@ -67,4 +67,14 @@
 
                 var m = new bbe.MantraContentController();
                 var f = new bbe.FormSubmission();
+                
+                $("#owl-demo").owlCarousel({
+ 
+                autoPlay: 3000, //Set AutoPlay to 3 seconds
+                
+                    items : 4,
+                    itemsDesktop : [1199,3],
+                    itemsDesktopSmall : [979,3]
+                
+                });
             });
